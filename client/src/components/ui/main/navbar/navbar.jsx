02bd7d223f369c7b63dd2e142345e72dcefbd4f6
Ailakks <hola@cortmnzz.com>
2022-10-12 17:01:18 +0200

    improved navbar design

--- client/src/components/ui/main/navbar/navbar.jsx
+++ client/src/components/ui/main/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import SearchBar from "../../input/search-bar";
-import {Button} from "@douyinfe/semi-ui";
+import {Button, Tooltip} from "@douyinfe/semi-ui";
 
 import style from "./navbar.module.css";
 
@@ -7,8 +7,18 @@ export default function Navbar() {
 
     return (
         <div className={style["navbar"]}>
+            <h1>Widdlo</h1>
             <SearchBar />
-            <Button>Iniciar sesión</Button>
+            <div>
+                <Tooltip content="Aplicación móvil">
+                    <Button className="main" icon={<i className="fa-light fa-mobile-button" />} aria-label="App"/>
+                </Tooltip>
+                <Tooltip content="Recursos">
+                    <Button className="main" icon={<i className="fa-light fa-book" />} aria-label="Help"/>
+                </Tooltip>
+                <Button className="main">Iniciar sesión</Button>
+                <Button className="main">Iniciar sesión</Button>
+            </div>
         </div>
     );
 }
