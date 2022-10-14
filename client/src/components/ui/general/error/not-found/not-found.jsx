7e0b1939ca2378fedc40cd879160add845ee9271
Ailakks <hola@cortmnzz.com>
2022-10-14 23:34:29 +0200

    improved error page design

--- client/src/components/ui/general/error/not-found/not-found.jsx
+++ client/src/components/ui/general/error/not-found/not-found.jsx
@@ -1,12 +1,13 @@
-import {Fragment} from "react";
-
 import style from "./not-found.module.css";
 
 export default function NotFound() {
 
     return (
-        <Fragment>
-            <h1>¡Nada por aquí!</h1>
-        </Fragment>
+        <div className={style["container"]}>
+            <div className={style["box"]}>
+                <h1>¡Nada por aquí!</h1>
+                <p>Hemos buscado por todas partes, de esquina a esquina, incluso por debajo de los sofás, pero no hemos encontrado nada.</p>
+            </div>
+        </div>
     );
 }
