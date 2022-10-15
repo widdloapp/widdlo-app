c8ff56efc036252032dce92e9923e30fc44bde6e
Ailakks <hola@cortmnzz.com>
2022-10-15 17:04:56 +0200

    improved overlay modal design

--- client/src/components/ui/main/account/modal/auth/auth-modal.jsx
+++ client/src/components/ui/main/account/modal/auth/auth-modal.jsx
@@ -19,19 +19,15 @@ export default function AuthModal() {
         <Fragment>
             <button className="main" onClick={onOpen}>Inicia sesión o regístrate</button>
 
-            <Modal  className={style["modal"]}closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent className={style["modal-overlay"]}>
-                    <ModalCloseButton />
+            <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay className={style["modal-overlay"]} />
+                <ModalContent className={style["modal-content"]}>
+                    <ModalCloseButton className={style["modal-close-button"]} />
                     <ModalBody>
                         <h1>Comienza tu aventura.</h1>
                         <hr className="spaced" />
                         <p>a</p>
                     </ModalBody>
-
-                    <ModalFooter>
-                        <p>¿Necesitas ayuda?</p>
-                    </ModalFooter>
                 </ModalContent>
             </Modal>
         </Fragment>
