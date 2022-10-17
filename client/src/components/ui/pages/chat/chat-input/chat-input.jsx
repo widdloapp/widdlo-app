5944f98ce6f03e255a4ce33836eb009748d5bf50
Ailakks <hola@cortmnzz.com>
2022-10-17 16:36:08 +0200

    added submit clear

--- client/src/components/ui/pages/chat/chat-input/chat-input.jsx
+++ client/src/components/ui/pages/chat/chat-input/chat-input.jsx
@@ -1,10 +1,17 @@
 import style from "./chat-input.module.css";
+import {useState} from "react";
 
 export default function ChatInput(props) {
 
+    const [body, setBody] = useState('');
+
     return (
-        <form onSubmit={props.submit} className={style["wrapper"]}>
-            <textarea name="body" placeholder="¡Añade un mensaje a este chat!" />
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            props.submit(event)
+            setBody('')
+        }} className={style["wrapper"]}>
+            <textarea value={body} onChange={event => setBody(event.target.value)} type="a" name="body" placeholder="¡Añade un mensaje a este chat!" />
             <div className={style["buttons-wrapper"]}>
                 <div>
                     <p>200 caracteres máximo.</p>
