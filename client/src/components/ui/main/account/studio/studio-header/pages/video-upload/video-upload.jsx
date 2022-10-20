317ef18d887341304cc1a0d7f9c07caf1d7e5a71
Ailakks <hola@cortmnzz.com>
2022-10-20 18:33:20 +0200

    added tip sidebar

--- client/src/components/ui/main/account/studio/studio-header/pages/video-upload/video-upload.jsx
+++ client/src/components/ui/main/account/studio/studio-header/pages/video-upload/video-upload.jsx
@@ -1,5 +1,7 @@
 import style from "./video-upload.module.css";
 import {api} from "../../../../../../../../shared/utils/token/api.js";
+import TipsSidebar from "../../../components/tips-sidebar/tips-sidebar";
+import {Fragment} from "react";
 
 export default function VideoUpload() {
 
@@ -18,12 +20,24 @@ export default function VideoUpload() {
 
     return (
         <div className={style["wrapper"]}>
-            <p>¡Estamos deseando ver tu vídeo circulado por Widdlo!<br />Antes de nada, asegúrate que tu vídeo cumple con las <a href="https://google.es">Normas de la Comunidad</a>.</p>
-            <form className={style["wrapper"]} onSubmit={postVideo}>
-                <input required={true} name="title" className="main" type="text" placeholder="Título" />
-                <textarea name="description" className="main" placeholder="Descripción" />
-                <input type="submit" value="Publicar" className="main" />
-            </form>
+            <TipsSidebar sidebar={
+                <div>
+                    <div className="tip-box">
+                        <h1>Consigue mejores resultados</h1>
+                        <hr className="spaced" />
+                        <p>Consulta en este <a>artículo de nuestro blog</a> buenas prácticas a seguir para mejorar las estadísticas de tus vídeos.</p>
+                    </div>
+                </div>
+            } content={
+                <div>
+                    <p>¡Estamos deseando ver tu vídeo circulado por Widdlo!<br />Antes de nada, asegúrate que tu vídeo cumple con las <a href="https://google.es">Normas de la Comunidad</a>.</p>
+                    <form className={style["wrapper"]} onSubmit={postVideo}>
+                        <input required={true} name="title" className="main" type="text" placeholder="Título" />
+                        <textarea name="description" className="main" placeholder="Descripción" />
+                        <input type="submit" value="Publicar" className="main" />
+                    </form>
+                </div>
+            } />
         </div>
     );
 }
