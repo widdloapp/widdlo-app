5a3960e2c8d3484cba9f782c0736bfeb55410368
Ailakks <hola@cortmnzz.com>
2022-10-20 20:39:27 +0200

    video manage improvement

--- client/src/components/ui/main/account/studio/studio-header/pages/video-manager/video-manager.jsx
+++ client/src/components/ui/main/account/studio/studio-header/pages/video-manager/video-manager.jsx
@@ -1,13 +1,31 @@
 import style from "./video-manager.module.css";
 import {Link} from "react-router-dom";
+import InlineVideoGrid from "../../../../../../videos/video-grid/inline-grid/inline-video-grid";
+import TipsSidebar from "../../../components/tips-sidebar/tips-sidebar.jsx";
 
 export default function VideoManager() {
 
     return (
         <div className={style["wrapper"]}>
-            <Link to="/studio/upload">
-                <button className="important">Nuevo vídeo</button>
-            </Link>
+            <TipsSidebar sidebar={
+                <div className="tip-wrapper">
+                    <Link to="/studio/upload">
+                        <button className="important full">Nuevo vídeo</button>
+                    </Link>
+                    <Link to="/studio/upload">
+                        <button className="secondary full">Asesoría para creadores</button>
+                    </Link>
+                    <div className="tip-box">
+                        <h1>¿Cómo gestionar mis vídeos?</h1>
+                        <hr className="spaced" />
+                        <p>Obtén consejos <a>aquí</a> sobre cómo mejorar la visibilidad de tu canal y su actividad.</p>
+                    </div>
+                </div>
+            } content={
+                <div>
+                    <InlineVideoGrid />
+                </div>
+            } />
         </div>
     );
 }
