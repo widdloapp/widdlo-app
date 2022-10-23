cc95a8a1ca9e2d9fa2fa447e02c3c2d986dc66e7
Ailakks <hola@cortmnzz.com>
2022-10-23 17:00:12 +0200

    added follow button

--- client/src/components/ui/pages/channel/components/follow-button/follow-button.jsx
+++ client/src/components/ui/pages/channel/components/follow-button/follow-button.jsx
@@ -1,8 +1,25 @@
+import {Fragment, useEffect, useState} from "react";
+import {api} from "../../../../../../shared/utils/token/api.js";
+
 export default function FollowButton(props) {
 
-    return (
-        <OrderContext.Provider value={props.order}>
-            <SidebarLayout sidebar={<ChannelSidebar />} content={<MainChannel />} />
-        </OrderContext.Provider>
-    );
+    const [loaded, setLoaded] = useState(false);
+    const [following, setFollowing] = useState(false);
+
+    useEffect(() => {
+        api('GET', `follow/${props.user}`).then(res => {
+            if () {
+                setFollowing(res.channel);
+            }
+            setLoaded(true);
+        })
+    }, []);
+
+    if (loaded) {
+        return (
+            <Fragment>
+                {following ? <button className="main">Dejar de seguir</button> : <button className="main">Seguir</button>}
+            </Fragment>
+        );
+    }
 }
