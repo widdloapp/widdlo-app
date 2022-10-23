bbee665228d391c40fbe927d6e7add441b923770
Ailakks <hola@cortmnzz.com>
2022-10-23 18:01:06 +0200

    added comment popover

--- client/src/components/ui/videos/comments/comment-box/comment-list/comment-list.jsx
+++ client/src/components/ui/videos/comments/comment-box/comment-list/comment-list.jsx
@@ -1,9 +1,11 @@
 import style from "./comment-list.module.css";
 import {Drawer, useDisclosure} from "@chakra-ui/react";
 import {Link, useNavigate, useParams} from "react-router-dom";
-import {useEffect} from "react";
+import React, {useEffect} from "react";
 import CommentDrawer from "../../comment-drawer/comment-drawer.jsx";
 import NoComments from "../no-comments/no-comments.jsx";
+import ChannelPopup from "../../../../pages/channel/channel-popup/channel-popup.jsx";
+import PopoverWrapper from "../../../../pages/channel/sidebar/popover-wrapper/popover-wrapper.jsx";
 
 export default function CommentList(props) {
 
@@ -31,7 +33,7 @@ export default function CommentList(props) {
                 {props.data.comments.length > 0 ?
                     props.data.comments.map((comment, key) =>
                         <div key={key} className={style["comment-box"]}>
-                            <img className="avatar" src={comment.author.avatar} />
+                            <PopoverWrapper trigger={<Link><img className="avatar undraggable unselectable" src={comment.author.avatar} /></Link>} content={<ChannelPopup id={comment.author.id} />} />
                             <div className={style["content"]}>
                                 <p><mark>{comment.author.name}</mark> hace 1 día</p>
                                 <p>{comment.body}</p>
