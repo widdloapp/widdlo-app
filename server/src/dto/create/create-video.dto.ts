c537e8429229beb45903bc8b69843effd291e78c
Ailakks <hola@cortmnzz.com>
2022-10-24 17:12:09 +0200

    api body size tests

--- server/src/dto/create/create-video.dto.ts
+++ server/src/dto/create/create-video.dto.ts
@@ -1,14 +1,6 @@
 import {IsBooleanString, IsNotEmpty, IsOptional, IsString, MaxLength} from "class-validator";
 
 export class CreateVideoDto {
-
-    readonly title: string;
-
-    @IsString()
-    @MaxLength(300)
-    @IsOptional()
-    readonly description: string;
-
     @IsOptional()
     @IsBooleanString()
     readonly hidden: boolean;
