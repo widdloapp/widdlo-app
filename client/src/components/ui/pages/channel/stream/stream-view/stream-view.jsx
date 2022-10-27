9c73190ea61e43bc59e5e0ddbb0743d97b75e0d9
Ailakks <hola@cortmnzz.com>
2022-10-27 21:24:53 +0200

    added stream player

--- client/src/components/ui/pages/channel/stream/stream-view/stream-view.jsx
+++ client/src/components/ui/pages/channel/stream/stream-view/stream-view.jsx
@@ -3,11 +3,12 @@ import {useEffect, useState} from "react";
 import {api} from "../../../../../../shared/utils/token/api.js";
 
 import style from "./stream-view.module.css";
+import ReactHlsPlayer from "react-hls-player";
 
 export default function StreamView() {
 
     const controls = ['play', 'rewind', 'restart', 'mute', 'volume', 'pip', 'airplay', 'fullscreen'];
-    
+
     const [data, setData] = useState([]);
 
     useEffect(() => {
@@ -24,7 +25,7 @@ export default function StreamView() {
         <div className={style["wrapper"]}>
             <div className={style["video-wrapper"]}>
                 <link rel="stylesheet" href="https://cdn.plyr.io/3.7.2/plyr.css"/>
-                <video className="video-player" autoPlay playsInline controls src=""/>
+                <ReactHlsPlayer className="video-player" autoPlay={true} src="http://localhost:8888/uabxg8xgyqutf8p5epc1kxbv01ti38lkzil9asfr8501jpjjm2ydgm9gmvbia82rtf8ov9pf/index.m3u8" />
             </div>
             <h1>aaaa</h1>
         </div>
