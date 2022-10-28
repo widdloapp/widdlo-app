337c260943e937476e2486c4a8bd998940ec5599
Ailakks <hola@cortmnzz.com>
2022-10-28 18:51:01 +0200

    fixed channel controller dto

--- server/src/channel/channel.controller.ts
+++ server/src/channel/channel.controller.ts
@@ -4,19 +4,20 @@ import {CreateChannelDto} from "../dto/create/create-channel.dto";
 import {ChannelInfoDto} from "../dto/create/channel-info.dto";
 import {StreamService} from "../stream/stream.service";
 import {UpdateChannelDto} from "../dto/update/update-channel.dto";
+import {KeyQueryDto} from "../dto/get/key-query.dto";
 
 @Controller('channel')
 export class ChannelController {
     constructor(private readonly channelService: ChannelService, private readonly streamService: StreamService) { }
 
     @Post()
-    async createChannel(@Res() response, @Body() createChannelDto: CreateChannelDto) {
+    async createChannel(@Res() response, @Body() createChannelDto: CreateChannelDto, @Body() keyQueryDto: KeyQueryDto) {
         createChannelDto.user = response.locals.user;
 
         try {
             const channel = await this.channelService.createChannel(createChannelDto);
 
-            await this.streamService.createStream(channel._id)
+            await this.streamService.createStream(keyQueryDto);
 
             return response.status(HttpStatus.OK).json({
                 message: 'Channel successfully created.', channel
