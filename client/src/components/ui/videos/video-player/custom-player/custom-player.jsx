26086b6c9ff1393fddcd02784db416339a88928e
Ailakks <hola@cortmnzz.com>
2022-10-30 16:34:42 +0100

    added player controls logic

--- client/src/components/ui/videos/video-player/custom-player/custom-player.jsx
+++ client/src/components/ui/videos/video-player/custom-player/custom-player.jsx
@@ -1,16 +1,20 @@
 import style from "./custom-player.module.css";
+import {useEffect} from "react";
 
 export default function CustomPlayer(props) {
 
+    let player;
+    useEffect(() => player = document.getElementById('video-player'));
+
     return (
         <div className={style["container"]}>
-            <video className={style["player"]} src={props.source} />
+            <video id="video-player" className={style["player"]} src={props.source} />
             <div className={style["wrapper"]}>
                 <div className={style["header"]}>
                     <h1>a</h1>
                 </div>
                 <div className={style["controls"]}>
-                    <button className={style["control"]}><i className="fa-solid fa-play" /></button>
+                    <button onClick={() => player.play()} className={style["control"]}><i className="fa-solid fa-play" /></button>
                 </div>
             </div>
         </div>
