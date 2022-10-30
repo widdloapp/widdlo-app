b7f38135e7b006726eb0178a166d193d0c9dfa2d
Ailakks <hola@cortmnzz.com>
2022-10-30 17:21:55 +0100

    added volume control

--- client/src/components/ui/videos/video-player/custom-player/custom-player.jsx
+++ client/src/components/ui/videos/video-player/custom-player/custom-player.jsx
@@ -45,12 +45,13 @@ export default function CustomPlayer(props) {
                 <div className={style["controls"]}>
                     <button onClick={() => alterPlay()} className={style["control"]}>{playing ? <i className="fa-solid fa-pause" /> : <i className="fa-solid fa-play" />}</button>
                     <input className={style["video-range"]} id="video-range" type="range" onChange={() => setVideoProgress()} />
-                    <button onClick={() => alterFullScreen()} className={style["control"]}>{fullScreen ?
-                        <i className="fa-sharp fa-solid fa-down-left-and-up-right-to-center" /> :
-                        <i className="fa-sharp fa-solid fa-up-right-and-down-left-from-center" />}</button>
+                    <input className={style["video-range"]} id="video-volume" type="range" onChange={() => setVideoProgress()} />
                     <button onClick={() => alterPictureInPicture()} className={style["control"]}>
                         <i className="fa-sharp fa-solid fa-share-from-square" />
                     </button>
+                    <button onClick={() => alterFullScreen()} className={style["control"]}>{fullScreen ?
+                        <i className="fa-sharp fa-solid fa-down-left-and-up-right-to-center" /> :
+                        <i className="fa-sharp fa-solid fa-up-right-and-down-left-from-center" />}</button>
                 </div>
             </div>
         </div>
