595f2a29448f9004ff0606aeab6f0768efafaa27
Ailakks <hola@cortmnzz.com>
2023-02-20 16:19:54 +0100

    cleanup

--- client-3/src/shared/utils/token/token.js
+++
@@ -1,5 +0,0 @@
-export const getStoredToken = () => localStorage.getItem('token');
-
-export const storeToken = token => localStorage.setItem('token', token);
-
-export const removeStoredToken = () => localStorage.removeItem('token');
